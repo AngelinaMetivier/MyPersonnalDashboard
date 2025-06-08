111c0584926b1c79b17d8edee89916d5c27baae2
AngelinaMetivier <metivier.angelina@hotmail.com>
2025-06-08 13:37:47 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-// GERER LA TO DO LIST
-
 let checkBoxes = document.querySelectorAll("input[type=checkbox]");
 let inputTexts = document.querySelectorAll("#to-do-list input[type=text]");
 
@@ -17,26 +14,5 @@ checkBoxes.forEach((checkBox, index) => {
         if (inputText.value === "") {
             checkBox.checked = false;
         }
-    });
-=======
-// GERER LA TO DO LIST
-
-let checkBoxes = document.querySelectorAll("input[type=checkbox]");
-let inputTexts = document.querySelectorAll("#to-do-list input[type=text]");
-
-checkBoxes.forEach((checkBox, index) => {
-    checkBox.addEventListener("click", () => {
-        let inputText = inputTexts[index];
-
-        // Si la case est cochée, vider le champ de texte
-        if (checkBox.checked) {
-            inputText.value = "";
-        }
-
-        // Vérifier si le champ de texte est vide, et décocher la case
-        if (inputText.value === "") {
-            checkBox.checked = false;
-        }
-    });
->>>>>>> 9c0d6912b2b01c2228bf82547c23977200bee0d4
+    })
 });
